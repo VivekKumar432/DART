cd3c64e011c88b1c646c5d588b33b75e45263fd4
VivekKumar432 <76239891+VivekKumar432@users.noreply.github.com>
2022-03-27 14:52:54 +0530

    Update anagram.dart

--- anagram.dart
+++ anagram.dart
@@ -31,18 +31,4 @@ void main() {
     print("NOT anagram");
   }
 }
-  /*int len2 = b.length;
-  bool res = true;
-  if (len1 == len2) {
-    for (int i = 0; i < len1; i++) {
-      if (a[i] != b[i]) {
-        res = false;
-      }
-    }
-  } else {
-    print("The string doesn't have same number of characters");
-  }
-  if (res) {
-    print("The ");
-  }*/
 
