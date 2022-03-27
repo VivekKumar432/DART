5f88469f0cda7419021326da65c6df9ee092409d
VivekKumar432 <76239891+VivekKumar432@users.noreply.github.com>
2022-03-27 14:52:25 +0530

    Add files via upload

---
+++ anagram.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the first string");
+  String a = (stdin.readLineSync()!);
+  print("Please enter the second string");
+  String b = (stdin.readLineSync()!);
+  int len1 = a.length;
+  int len2 = b.length;
+  List a1 = a.split('');
+  List b1 = b.split('');
+  bool q = true;
+  a1.sort();
+  b1.sort();
+  print(a1);
+  print(b1);
+  print(len1);
+  if (len1 == len2) {
+    for (int i = 0; i < len1; i++) {
+      if (a1[i] == b1[i]) {
+      } else {
+        q = false;
+      }
+    }
+    if (q) {
+      print("Anagram");
+    } else {
+      print("Not Anagram");
+    }
+  } else {
+    print("NOT anagram");
+  }
+}
+  /*int len2 = b.length;
+  bool res = true;
+  if (len1 == len2) {
+    for (int i = 0; i < len1; i++) {
+      if (a[i] != b[i]) {
+        res = false;
+      }
+    }
+  } else {
+    print("The string doesn't have same number of characters");
+  }
+  if (res) {
+    print("The ");
+  }*/
+
